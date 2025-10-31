ebff310ff0f8de1235b8340657335d35d37e91d4
Aman Joshi <amanjoshi16011997@gmail.com>
2025-10-31 18:19:38 +0530

    password change success

--- hrms-app/apps/frontend-ui-monorepo/src/features/auth/pages/PasswordChangedPage.jsx
+++ hrms-app/apps/frontend-ui-monorepo/src/features/auth/pages/PasswordChangedPage.jsx
@@ -1,3 +1,93 @@
-export default function PasswordChangedPage() {
-  return <h1 style={{ textAlign: "center", marginTop: "100px" }}>Password Changed Page</h1>;
-}
+import React from "react";
+import {
+  Box,
+  Button,
+  Image,
+  Input,
+  Text,
+  Link,
+  Checkbox,
+  Grid,
+  VStack,
+  HStack,
+  Heading,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/react";
+import passSucImg from "../../../assets/passSucImg.png";
+import companyLogo from "../../../assets/hankukLogo.png";
+import successIcon from "../../../assets/successIcon.png";
+
+const PasswordChangedPage = () => {
+  return (
+    <Grid
+      templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+      h="100vh"
+      w="100vw"
+    >
+      {/* Left Section - Login Form */}
+      <Box as="section" p={10} w={{ base: "100%", md: "auto" }}>
+        <Grid
+          h="100%"
+          templateRows="auto 1fr"
+          gap={8}
+          alignContent="center"
+          maxW="400px"
+          mx="auto"
+        >
+          {/* Logo */}
+          <Box justifySelf="center">
+            <Image src={companyLogo} w="18.375em" h="6.5rem" />
+          </Box>
+
+          {/* Form Content */}
+          <VStack mt={10} spacing={8} align="stretch">
+            {/* Form Fields */}
+              <Box >
+                <Image justifySelf="center" src={successIcon} h="35vh" fit='cover' />
+              </Box>
+
+              <Heading
+                bgGradient="linear(to-r, #307DC5, #BDBBB9)"
+                bgClip="text"
+                as="h1"
+                size="lg"
+                mb={2}
+      
+                textAlign='center'
+              >
+                Congratulations
+              </Heading>
+
+              <Text                 textAlign='center'
+>
+                You have successfully changed your password.
+              </Text>
+
+              {/* Sign In Button */}
+              <Button
+                size="lg"
+                w="100%"
+                mt={4}
+                bgGradient="linear(to-r, #307DC5, #BDBBB9)"
+                color="white"
+                _hover={{
+                  bgGradient: "linear(to-r, #276AAB, #A9A7A5)",
+                  opacity: 0.9,
+                }}
+              >
+                Back To Login
+              </Button>
+          </VStack>
+        </Grid>
+      </Box>
+
+      {/* Right Section - Image */}
+      <Box as="section" display={{ base: "none", md: "block" }}>
+        <Image src={passSucImg} height="100%" width="100%" objectFit="cover" />
+      </Box>
+    </Grid>
+  );
+};
+
+export default PasswordChangedPage;
